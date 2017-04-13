dfc87248586fb07f0ed5cedc6ed13fceec6f2b9d
Jason Sharp <JSharp1@users.noreply.github.com>
2017-04-13 09:20:41 +0100

    Update training_carServerGpio.py

--- training_carServerGpio.py
+++ training_carServerGpio.py
@@ -12,7 +12,7 @@ print 'starting GPIO server'
 # start a socket and listen for clients (0.0.0.0 
 # on all interfaces)
 server_socket = socket.socket()
-server_socket.bind(('0.0.0.0', 44465))
+server_socket.bind(('0.0.0.0', 44470))
 server_socket.listen(0)
 print "socket is listening for a client"
 conn, addr = server_socket.accept()
@@ -28,23 +28,19 @@ pi.set_mode(SERVO_PIN, pigpio.OUTPUT)
 pi.set_mode(MOTOR_PIN, pigpio.OUTPUT)
 d1 = 0
 d2 = 0
-sampleNo = 0
-label_array = np.zeros((1, 2))
+label_array = np.zeros((1, 3))
 
-# def saveData():
-# 	global sampleNo
-# 	sampleNo = sampleNo + 1
-# 	# print sampleNo
-# 	filePath_1 = "data/tempImg.jpg"
-# 	filePath_2 = "data/" + "{0:04d}.jpg".format(sampleNo)
-# 	shutil.copyfile(filePath_1,filePath_2)
-# 	global d1,d2,label_array
-# 	label_array = np.vstack((label_array, np.array([d1,d2])))
-
+def saveData():
+	filePath_1 = "data/tempImg.jpg"
+	timestr = time.strftime("%Y%m%d-%H%M%S")#get the time and date in format
+	filePath_2 = "data/" + timestr + ".jpg"
+	shutil.copyfile(filePath_1,filePath_2)
+	global d1,d2,label_array
+	label_array = np.vstack((label_array, np.array([timestr,d1,d2])))
 
 
 def main():
-	# saveFlag = True
+	saveFlag = False
 	# t1 = time.time()
 	# t2 = .5 + t1
 	try:
@@ -58,10 +54,8 @@ def main():
 			# t.start()
 			#print 'save'
 			# inc = 0
-			# if saveFlag == True:
-			# 	saveData()
-			# saveData()
-			time.sleep(.05)
+
+			time.sleep(.1)
 			data = conn.recv(20)
 			if data:
 				global d1,d2
@@ -84,6 +78,10 @@ def main():
 					print 'servo: ', d1,' ','motor: ', d2
 					pi.set_servo_pulsewidth(SERVO_PIN, int(d1))
 					pi.set_PWM_dutycycle(MOTOR_PIN,int(d2))
+
+				if saveFlag == True:
+					saveData()
+
 			else:
 				print 'client closed connection'
 				break
