c6a5c61ac57ab484627c5ca5666e1018a4a0d29c
Jason Sharp <JSharp1@users.noreply.github.com>
2017-04-29 21:49:13 +0100

    Update load CSV data into dictionary.py

--- trafic sign classifier/load CSV data into dictionary.py
+++ trafic sign classifier/load CSV data into dictionary.py
@@ -25,17 +25,10 @@ coords 4D array: containing orignal coords of object in image (x1,y1,x2,y2)
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-
+Flag = True
 #variables to increment through data
-numberOfClasses = 42#43 classes
-numberOfFeatures = 0#init as zero for now
-classIdx, featIdx = 0,0
-# dynamic file path
-folderPath = "GTSRB/Final_Training/Images/"
+numberOfClasses = 42#43 classes/folders to loop through
 names = ["Filename", "Width", "Height", "Roi.X1", "Roi.Y1", "Roi.X2", "Roi.Y2", "ClassId"]	
-
-
-data_dict = {}
 features = np.zeros((1,3072),dtype=np.int8)#32x32)
 labels = np.zeros((1, 3),dtype=np.int8)
 sizes = np.zeros((1,2),dtype=np.int8)
@@ -61,26 +54,80 @@ def preprocess(rgbImage):
 	return yuvImage
 
 def main():
-	for i in range(numberOfClasses):#loop through each folder
+	folderPath = "GTSRB/Final_Training/Images/"
+	if Flag == True:
+		for i in range(1):#loop through each folder
+			classCounter = "{0:05d}".format(i)#class increment var
+			csvCounter = "GT-" + classCounter#csv file name
+			csvPATH = str(folderPath + classCounter + "/" + csvCounter + ".csv")
+			print "class", i
+			print "***** saving training data from " + csvPATH
+			data = np.genfromtxt(csvPATH, dtype=None, delimiter=';', skip_header=0, names=True)
+			print "converting ", len(data), " samples"
+			for j in range(len(data)):#loop over each row of data #len(data)
+				#j = 28
+				#save labels
+				s = data['Filename'][j]
+				#split file name into set and idx
+				s1, s2  = s.split("_")
+				s3, s4 = s2.split(".")
+				x = data['ClassId'][j]
+				global labels
+				labels = np.vstack((labels, np.array([int(x),int(s1),int(s3)])))
+				# print s,x,s1,s3,labels[j+1]
+				#save img sizes
+				h = data['Height'][j]
+				w = data['Width'][j]
+				global sizes
+				sizes = np.vstack((sizes, np.array([h,w])))
+				# print h,w,sizes[j+1]
 
-		classCounter = "{0:05d}".format(i)#class increment var
-		csvCounter = "GT-" + classCounter#csv file name
-		csvPATH = str(folderPath + classCounter + "/" + csvCounter + ".csv")
-		print "class", i
-		print "***** loading data from " + csvPATH
+				#save coords
+				x1,x2,y1,y2 = data['RoiX1'][j], data['RoiX2'][j], data['RoiY1'][j], data['RoiY2'][j]
+				global coords
+				coords = np.vstack((coords, np.array([x1,x2,y1,y2])))
+				# print j,x1,coords[j+1][0]
+
+				#save images
+				imageCounter_1 = "{0:05d}".format(int(s1))#the set of image in class
+				imageCounter_2 = "{0:05d}".format(int(s3))#the number of image in set
+				imagePATH = str(folderPath + classCounter + "/" + imageCounter_1 + "_"+ imageCounter_2 + ".ppm")
+				# print "loading img from " + imagePATH
+				src = cv2.imread(imagePATH)
+				yuvImage = preprocess(src)
+
+				#resize the image
+				ratio = 32.0 / src.shape[1]
+				dim = (32, int(src.shape[0] * ratio))
+				resized = cv2.resize(yuvImage, dim, interpolation=cv2.INTER_NEAREST)
+				resized1 = cv2.resize(resized, (32,32))
+				img_array = resized1.reshape(1, 3072).astype(np.uint8)
+				global features
+				features = np.vstack((features, img_array))
+				# img_mat = np.reshape(img_array,(32,32,3)).astype(np.uint8)
+				# cv2.waitKey(0) 
+				# cv2.destroyAllWindows()
+				# print j
+
+	np.savez('trainingData_dict.npz', features=features, labels=labels, sizes= sizes, coords = coords)
+	elif Flag == False:
+		print Flag
+		folderPath = "GTSRB/Final_Test/Images"
+		csvPATH = str(folderPath + "/GT-final_test.csv")
+		print "***** saving test data from " + csvPATH
 		data = np.genfromtxt(csvPATH, dtype=None, delimiter=';', skip_header=0, names=True)
 		print "converting ", len(data), " samples"
 		for j in range(len(data)):#loop over each row of data #len(data)
-			#j = 28
-			#save labels
-			s = data['Filename'][j]
-			#split file name into set and idx
-			s1, s2  = s.split("_")
-			s3, s4 = s2.split(".")
+
+			#save labels: class,index
+			s = data['Filename'][j]#string
+			#split file name into idx
+			s1, s2 = s.split(".")
 			x = data['ClassId'][j]
 			global labels
-			labels = np.vstack((labels, np.array([int(x),int(s1),int(s3)])))
+			labels = np.vstack((labels, np.array([int(x),int(s1),0])))
 			# print s,x,s1,s3,labels[j+1]
+
 			#save img sizes
 			h = data['Height'][j]
 			w = data['Width'][j]
@@ -96,9 +143,8 @@ def main():
 
 			#save images
 			imageCounter_1 = "{0:05d}".format(int(s1))#the set of image in class
-			imageCounter_2 = "{0:05d}".format(int(s3))#the number of image in set
-			imagePATH = str(folderPath + classCounter + "/" + imageCounter_1 + "_"+ imageCounter_2 + ".ppm")
-			# print "loading img from " + imagePATH
+			imagePATH = str(folderPath + "/" + imageCounter_1 + ".ppm")
+			print "loading img from " + imagePATH + " converting ", len(data), " samples"
 			src = cv2.imread(imagePATH)
 			yuvImage = preprocess(src)
 
@@ -110,35 +156,32 @@ def main():
 			img_array = resized1.reshape(1, 3072).astype(np.uint8)
 			global features
 			features = np.vstack((features, img_array))
-			# img_mat = np.reshape(img_array,(32,32,3)).astype(np.uint8)
-			# cv2.imshow("rgb", src)
-			# cv2.imshow("yuv CLAHE",cv2.cvtColor(yuvImage,cv2.COLOR_YUV2BGR))
+			img_mat = np.reshape(img_array,(32,32,3)).astype(np.uint8)
+			#cv2.imshow("rgb", src)
 			# cv2.imshow("img_mat",cv2.cvtColor(img_mat,cv2.COLOR_YUV2BGR))
 			# cv2.imshow("rgb resized", cv2.cvtColor(resized,cv2.COLOR_YUV2BGR))
 			# print yuvImage.shape
 			# print resized.shape
 			# cv2.waitKey(0) 
+			# cv2.imshow("yuv CLAHE",cv2.cvtColor(yuvImage,cv2.COLOR_YUV2BGR))
+			# cv2.waitKey(0)     
 			# cv2.destroyAllWindows()
 			# print j
 
-
-
-
-
-		#get a random sample and display
-
-		# plt.figure()
-		# #src
-		# plt.subplot(1,2,1).set_title("SRC")
-		# plt.subplot(1,2,1).axes.get_xaxis().set_visible(False), plt.subplot(1,2,1).axes.get_yaxis().set_visible(False)
-		# plt.subplot(1,2,1).imshow(cv2.cvtColor(src,cv2.COLOR_BGR2RGB))
-		# #y channel after CLAHE
-		# plt.subplot(1,2,2).set_title("SRC CLAHE")
-		# plt.subplot(1,2,2).axes.get_xaxis().set_visible(False), plt.subplot(1,2,2).axes.get_yaxis().set_visible(False)
-		# plt.subplot(1,2,2).imshow(cv2.cvtColor(yuvImage,cv2.COLOR_YUV2RGB))
-		# plt.ion()
-		# # plt.pause(.001)
-		# plt.close(j)
+			#get a random sample and display
+			# plt.figure()
+			# #src
+			# plt.subplot(1,2,1).set_title("SRC")
+			# plt.subplot(1,2,1).axes.get_xaxis().set_visible(False), plt.subplot(1,2,1).axes.get_yaxis().set_visible(False)
+			# plt.subplot(1,2,1).imshow(cv2.cvtColor(src,cv2.COLOR_BGR2RGB))
+			# #y channel after CLAHE
+			# plt.subplot(1,2,2).set_title("SRC CLAHE")
+			# plt.subplot(1,2,2).axes.get_xaxis().set_visible(False), plt.subplot(1,2,2).axes.get_yaxis().set_visible(False)
+			# plt.subplot(1,2,2).imshow(cv2.cvtColor(yuvImage,cv2.COLOR_YUV2RGB))
+			# plt.ion()
+			# # plt.pause(.001)
+			# plt.close(j)
+		np.savez('testData_dict.npz', features=features, labels=labels, sizes= sizes, coords = coords)
 
 		# print npzfile["features"][3].shape
 		# temp = np.array(npzfile["features"][3])
@@ -160,7 +203,8 @@ def main():
 		# print data['Filename'].dtype
 		# print data_dict['Filename'][210]
 		# print data[1].dtype
-	np.savez('data_dict.npz', features=features, labels=labels, sizes= sizes, coords = coords)
+
+	
 	print '-' * 10
 	print "data saved"
 	npzfile = np.load('data_dict.npz', 'r')
@@ -168,6 +212,7 @@ def main():
 	print "keys: " + str(npzfile.files)
 	print "length of data " + str(len(npzfile["labels"])-1)#minus one as first row are zeros
 	print '-' * 10
- 
+
+
 if __name__ == '__main__':
 	main()
